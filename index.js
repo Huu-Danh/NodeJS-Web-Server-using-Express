d693754d2a52eeeb0c5f725755bf9e2c4330c559
Huu Danh <nguyentanhuudanhit>
2024-06-26 12:25:44 +0700

    update view user with use nodemon, lowdb, body

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 const app = express();
+const bodyParser = require('body-parser')  ;
 const port = 3333;
+ 
+const adapter = new FileSync('db.json');
+db = low(adapter);
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-var users = [
-  {id: 1, name: 'Danh'},
-  {id: 2, name: 'Tai'}
-];
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+var users = db.get('users').value();
 
 app.get('/', (req, res) => {
   res.render('index', {
@@ -27,10 +33,29 @@ app.get('/users/search', function(req, res){
   var matchedUsers = users.filter(function(user){
     return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
   });
-
+  
   res.render('users/index',{
     users:matchedUsers,
   });
+});
+
+app.get('/users/create', function(req, res){
+  res.render('users/create');
+});
+
+app.get('/users/:id', function(req, res){
+  var id = parseInt(req.params.id);
+
+  var user = db.get('users').find({id: id}).value();
+
+  res.render('users/view', {
+    user: user
+  });
+});
+
+app.post('/users/create', function(req, res){
+  db.get('users').push(req.body).write();
+  res.redirect('/users');
 });
 
 app.listen(port, () => {
