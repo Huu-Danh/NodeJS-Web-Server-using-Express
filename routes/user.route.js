5cc49b6eb472db83a881923f3718fbee36cbda1e
Huu Danh <nguyentanhuudanhit>
2024-07-17 22:13:56 +0700

    midleware in expressjs

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const controller = require('../controllers/user.controller');
 const router = express.Router()
-
+var validate = require('../validate/user.valdate')
 
 
 router.get('/', controller.index);
@@ -12,5 +12,5 @@ router.get('/create', controller.create);
 
 router.get('/:id', controller.get);
 
-router.post('/create', controller.postCreate);
+router.post('/create', validate.postCreate , controller.postCreate);
 module.exports = router;
