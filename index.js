8a2c241f6bad275c616d0b4eea898b884ff911ba
Huu Danh <nguyentanhuudanhit>
2024-07-20 00:26:30 +0700

    environment Variables

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+console.log(process.env);
 const express = require('express');
 const bodyParser = require('body-parser');
 var userRoutes = require('./routes/user.route');
@@ -12,7 +14,7 @@ app.set('views', './views');
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use(cookieParser('asdadaggrwqgsdexaaw'))
+app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
