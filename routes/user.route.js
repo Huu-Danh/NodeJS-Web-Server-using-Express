a34e60b86e8b3c22d939734200cc961ce7ec1f36
Huu Danh <nguyentanhuudanhit>
2024-07-21 21:57:25 +0700

    file upload use install multer

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const multer = require('multer');
 const controller = require('../controllers/user.controller');
 const router = express.Router();
 var validate = require('../validate/user.valdate');
-
+var upload = multer({dest: './public/uploads/'});
 
 router.get('/',controller.index);
 
@@ -14,5 +15,5 @@ router.get('/create', controller.create);
 
 router.get('/:id', controller.get);
 
-router.post('/create', validate.postCreate , controller.postCreate);
+router.post('/create', upload.single('avatar'), validate.postCreate , controller.postCreate);
 module.exports = router;
